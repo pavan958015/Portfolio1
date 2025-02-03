b16a37f05a270c1affd999cbf25219383f7bd069
pavan <pavansoni210129@gmail.com>
2025-02-03 12:58:17 +0530

    Sheet Changes

--- js/script1.js
+++ js/script1.js
@@ -7,3 +7,15 @@ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
   .catch(error => console.error('Error!', error.message))
 });
+
+
+//Google Sheet connect code
+var scriptURL = "https://script.google.com/macros/s/AKfycbyKGziey4KlEAoceB4HkygpkQjaRgd_xDMBCP8WU2xBq_ZXZoVk21E4a9nMBMtmmP8w0Q/exec";
+var form = document.forms['google-sheet2'];
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+  .catch(error => console.error('Error!', error.message))
+});
+
